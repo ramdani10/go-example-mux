a57b132394bc9cbc6af11e4e79df81a43d0a1404
greenlab <dian.ramdani@greenlabgroup.com>
2020-07-09 19:48:46 +0700

    rest api with firebase store

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ramdani10/mux-example
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	cloud.google.com/go/firestore v1.2.0
+	github.com/gorilla/mux v1.7.4
+	google.golang.org/api v0.20.0
+)
--- server.go
+++ server.go
@@ -6,16 +6,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/ramdani10/mux-example/routes"
 )
 
 func main() {
 	router := mux.NewRouter()
 	const port string = ":8000"
+
 	router.HandleFunc("/", func(resp http.ResponseWriter, request *http.Request){
 		fmt.Fprintln(resp, "Up and Running")
 	})
-	router.HandleFunc("/post", getPost).Methods("GET")
-	router.HandleFunc("/post", addPost).Methods("POST")
+	router.HandleFunc("/post", routes.GetPosts).Methods("GET")
+	router.HandleFunc("/post", routes.AddPost).Methods("POST")
 
 
 	log.Println("server listening port", port)
